248a7dc3d33ca688799ccefe377831a018f036e6
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 22:51:10 +0600

    week1_task1

---
+++ Abdykanyeva_task1.py
@@ -0,0 +1,33 @@
+# Question 1
+
+# a = 5             
+# b = 10
+# c = 2
+
+# print((a+b)*c)
+# print(type(a))
+# print(type(b))
+# print(type(c))                
+
+#Question 2
+
+# a = 10               
+# b = 3
+
+# print(a%b)
+
+# Question3
+                 
+# age = input("How old are you?:")
+# print("You are age:" age)
+
+# Question 4
+
+# a = 10
+# b = 2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+  
